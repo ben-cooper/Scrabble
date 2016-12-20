19a3d125b3ec9ca13815cfd104235eeefcca936c
Ben Cooper <ben0cooper@gmail.com>
2016-12-20 17:58:15 -0500

    Added import (apparently necessary for windows)

--- word.cpp
+++ word.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <list>
 #include <string>
+#include <array>
 
 const int primes[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
 			47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
